702a65734794f8db450bf066f7293f4c98816af6
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-03-14 18:08:02 +0000

    First commit

---
+++ report_history.py
@@ -0,0 +1,80 @@
+from numpy import NaN
+import pandas as pd
+from datetime import datetime
+import matplotlib.dates
+import matplotlib.pyplot
+import numpy as np
+
+
+FILE_EXTENSION = "ReportHistory.xlsx"
+NEXT_TABLE = "Orders"
+
+
+class Data:
+    def __init__(self, xlsx_file_extension):
+        self.file = pd.read_excel(xlsx_file_extension, usecols="A:O", skiprows=6)
+
+        self.closed_position_time = pd.to_datetime(self.get_column("Time"))
+        self.closed_position_profits = self.get_column("Profit")
+        self.cumulative_profits = self.get_cumulative_profits(
+            self.closed_position_profits
+        )
+
+    def table_length(self, table):
+        for i, element in enumerate(table):
+            if element == NEXT_TABLE or str(element) == "nan":
+                return i
+
+    def get_column(self, column_name):
+        return self.file[column_name][: self.table_length(self.file[column_name])]
+
+    def get_cumulative_profits(self, profits):
+        cumulative_profits = []
+
+        for i in range(len(profits)):
+            if i == 0:
+                cumulative_profits.append(round(profits[i], 2))
+            else:
+                cumulative_profits.append(
+                    round(profits[i] + cumulative_profits[i - 1], 2)
+                )
+        return cumulative_profits
+
+    def plot_cumulative_profits(self):
+        matplotlib.pyplot.plot(self.closed_position_time, self.cumulative_profits)
+        matplotlib.pyplot.title("Cumulative P/L")
+        matplotlib.pyplot.show()
+
+        # matplotlib.pyplot.yscale("log")
+
+    def plot_losses(self):
+        losses = self.closed_position_profits.copy()
+        time = self.closed_position_time.copy()
+
+        for i, element in enumerate(self.closed_position_profits):
+            if element > 0:
+                losses.pop(i)
+                time.pop(i)
+
+        matplotlib.pyplot.plot(time, losses, linestyle="", marker="o")
+        matplotlib.pyplot.title("Losses")
+        matplotlib.pyplot.show()
+
+    def plot_wins(self):
+        wins = self.closed_position_profits.copy()
+        time = self.closed_position_time.copy()
+
+        for i, element in enumerate(self.closed_position_profits):
+            if element < 0:
+                wins.pop(i)
+                time.pop(i)
+
+        matplotlib.pyplot.plot(time, wins, linestyle="", marker="o")
+        matplotlib.pyplot.title("Wins")
+        matplotlib.pyplot.show()
+
+
+data = Data(FILE_EXTENSION)
+data.plot_cumulative_profits()
+data.plot_wins()
+data.plot_losses()
